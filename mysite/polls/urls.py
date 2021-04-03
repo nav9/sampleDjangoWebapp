6df1b480ea00e6113c67b4973b7ae78475c4e676
Navin <navinipe@gmail.com>
2021-04-03 18:47:14 +0530

    generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,16 +3,24 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'
+
 urlpatterns = [
-    #path('', views.index, name='indexx'),#name does not matter for the URL. Naming your URL lets you refer to it unambiguously from elsewhere in Django, especially from within templates. This powerful feature allows you to make global changes to the URL patterns of your project while only touching a single file
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),    
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+#urlpatterns = [
+#    #path('', views.index, name='indexx'),#name does not matter for the URL. Naming your URL lets you refer to it unambiguously from elsewhere in Django, especially from within templates. This powerful feature allows you to make global changes to the URL patterns of your project while only touching a single file
+#    # ex: /polls/
+#    path('', views.index, name='index'),
+#    # ex: /polls/5/
+#    path('<int:question_id>/', views.detail, name='detail'),
+#    # ex: /polls/5/results/
+#    path('<int:question_id>/results/', views.results, name='results'),
+#    # ex: /polls/5/vote/
+#    path('<int:question_id>/vote/', views.vote, name='vote'),    
+#]
 
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question
+from .models import Choice, Question
 from django.template import loader
 from django.http import Http404
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.views import generic
+from django.http import HttpResponseRedirect
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+        
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    context = {'latest_question_list': latest_question_list}
+#    return render(request, 'polls/index.html', context)
+    
 #    latest_question_list = Question.objects.order_by('-pub_date')[:5]
 #    template = loader.get_template('polls/index.html')
 #    context = {
@@ -18,9 +30,13 @@ def index(request):
 #    }
 #    return HttpResponse(template.render(context, request))
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+#def detail(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/detail.html', {'question': question})
     
 #    try:
 #        question = Question.objects.get(pk=question_id)
@@ -31,9 +47,13 @@ def detail(request, question_id):
 
 #    return HttpResponse("You're looking at question %s." % question_id)
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    
+#def results(request, question_id):
+#    response = "You're looking at the results of question %s."
+#    return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
