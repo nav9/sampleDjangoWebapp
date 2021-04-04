8306995f464f3fc69d60942c42ad20f6220ead51
Navin <navinipe@gmail.com>
2021-04-04 20:32:36 +0530

    admin page customizations

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# This is not secret for now because it's part of the tutorial code
 SECRET_KEY = '9dsdywr)+1m@cj939@p113+i!6^e0zn0$j#*%1it17ror0-na@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,13 +7,17 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     #It’s important to add __str__() methods to your models, not only for your own convenience when dealing with the interactive prompt, but also because objects’ representations are used throughout Django’s automatically-generated admin.
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now        
-
+        #these are function attributes. Since a function is an object, it can have members
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
