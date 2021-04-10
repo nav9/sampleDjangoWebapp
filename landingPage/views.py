6e5f4db32aafbb9c57f707e87c37a69188fce25a
Navin <navinipe@gmail.com>
2021-04-10 19:21:13 +0530

    logger working. Changed ampersand name to main

--- landingPage/views.py
+++ landingPage/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#import logging
-#
-#logger = logging.getLogger(__name__)
+import logging
+
+logger = logging.getLogger(__name__)
 
 def index(request):        
     context = {'_': None}
     r = str(request)
-    #logger.info(r)
+    logger.info("THIS IS FROM THE LOGGER"+r)
     #return HttpResponse("the request: ")
     #return render(request, 'landingPage/index.html', context)
     return render(request, 'landingPage/index.html', context)
